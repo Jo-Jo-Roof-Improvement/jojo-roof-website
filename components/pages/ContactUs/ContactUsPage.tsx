d49e286004659a7b9cf3ed784556d0d717300b6c
Paul Gradie <paul.e.gradie@gmail.com>
2022-01-22 20:27:31 +1100

    fix contact us page component props

--- components/pages/ContactUs/ContactUsPage.tsx
+++ components/pages/ContactUs/ContactUsPage.tsx
@@ -8,7 +8,7 @@ import { SectionTwo } from './sections/SectionTwo';
 
 export const ContactUsPage = () => {
     return (
-        <Layout pageTitle={`${COMPANYNAME} - Contact Us`} showWidget={false}>
+        <Layout pageTitle={`${COMPANYNAME} - Contact Us`}>
             <SectionOne />
             <SectionTwo />
             <SectionThree />
